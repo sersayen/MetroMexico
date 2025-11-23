013ceae17d04d090c72efe619ee237bd3784cf5a
jm-iglesias <jmiglesiasmolina04@gmail.com>
2025-11-23 23:13:49 +0100

    Update MetroApp.py

--- MetroApp.py
+++ MetroApp.py
@@ -2,22 +2,9 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 from PIL import Image, ImageTk
 from datetime import datetime
+import networkx as nx
 import busqueda_IA as modulo_IA  # Importamos el grafo y la función
-import os, sys
 
-def resource_path(relative_path):
-    #Obtiene el path absoluto de recursos, funciona en .py y en .exe
-        try:
-            base_path = sys._MEIPASS   # carpeta temporal creada por PyInstaller
-        except Exception:
-            base_path = os.path.abspath(".")
-        return os.path.join(base_path, relative_path)
-FRANJAS_HORARIAS = [
-    (7, 0, 9, 0),
-    (18, 0, 20, 0)
-]
-FACTOR_NORMAL = 1.0
-FACTOR_HORA_PUNTA = 1.2
 
 class MetroApp:
     def __init__(self, root):
@@ -54,15 +41,15 @@ class MetroApp:
         self.hora_entry = ttk.Entry(frame_controles, textvariable=self.hora_salida, width=10)
         self.hora_entry.grid(row=2, column=1, padx=5, pady=5, sticky="w")
 
-        self.status_label = tk.Label(frame_controles, text="Afluencia:", bg="lightyellow")
-        self.status_label.grid(row=3, column=0, columnspan=2, pady=5, sticky="ew")
+        self.afluencia_status_label = tk.Label(frame_controles, text="Afluencia:", bg="lightyellow")
+        self.afluencia_status_label.grid(row=3, column=0, columnspan=2, pady=5, sticky="ew")
 
 
         self.es_discapacitado_var = tk.BooleanVar(value=modulo_IA.es_discapacitado if hasattr(modulo_IA, 'es_discapacitado') else False)
         self.check_discapacidad = ttk.Checkbutton(frame_controles, text="Movilidad reducida", variable=self.es_discapacitado_var, command=self.toggle_discapacidad)
         self.check_discapacidad.grid(row=4, column=0, columnspan=2, pady=5, sticky="w")
-        self.status_label = tk.Label(frame_controles, text="Estaciones de dificil acceso: {Chapultepec, Insurgentes, Etiopia,\n Eugenia, Division del Norte, Coyoacan,\n Lazaro Cardenas}", bg="lightyellow")
-        self.status_label.grid(row=5, column=0, columnspan=2, pady=5, sticky="ew")
+        self.dificil_acceso_label = tk.Label(frame_controles, text="Estaciones de difícil acceso: {Chapultepec, Insurgentes, Etiopia,\n Eugenia, Division del Norte, Coyoacan, Lazaro Cardenas}", bg="lightyellow")
+        self.dificil_acceso_label.grid(row=5, column=0, columnspan=2, pady=5, sticky="ew")
 
         calcular_btn = tk.Button(frame_controles, text="Calcular ruta", command=self.calcular_ruta)
         calcular_btn.grid(row=6, column=0, columnspan=2, pady=10, sticky="ew")
@@ -74,7 +61,7 @@ class MetroApp:
         self.tiempo_label.grid(row=8, column=0, columnspan=2, pady=10, sticky="ew")
 
 
-        image_path = resource_path("Metro.png")
+        image_path = "Metro.png"
         image = Image.open(image_path)
         image_width, image_height = image.size
         max_width = int(screen_width // 2)
@@ -90,8 +77,6 @@ class MetroApp:
 
     def toggle_discapacidad(self):
         modulo_IA.es_discapacitado = self.es_discapacitado_var.get()
-        print(f"Penalizacion de accesibilidad: {'Activada' if modulo_IA.es_discapacitado else 'Desactivada'}")
-
 
     def hora_punta(self, hora_str):
         try:
@@ -99,12 +84,12 @@ class MetroApp:
             tiempo = h * 60 + m
             if not(0 <= h <= 23 and 0 <= m <= 59):
                 raise ValueError("Hora/minuto fuera de rango.")
-            for inicio_h, inicio_m, fin_h, fin_m in FRANJAS_HORARIAS:
+            for inicio_h, inicio_m, fin_h, fin_m in modulo_IA.FRANJAS_HORARIAS:
                 inicio_min = inicio_h * 60 + inicio_m
                 fin_min = fin_h * 60 + fin_m
                 if inicio_min <= tiempo < fin_min:
-                    return FACTOR_HORA_PUNTA, "Hora punta"
-            return FACTOR_NORMAL, "Afluencia normal"
+                    return modulo_IA.FACTOR_HORA_PUNTA, "Hora punta"
+            return modulo_IA.FACTOR_NORMAL, "Normal"
         except ValueError as e:
             messagebox.showerror("Error de hora", f"Formato invalido: {e}, usa HH:MM")
             return None, None
@@ -118,11 +103,22 @@ class MetroApp:
             self.resultado_label.config(text="Debe rellenar los campos de origen, destino y hora de salida.")
             self.tiempo_label.config(text="")
             return
+        nodos_validos = modulo_IA.Graph.nodes
+
+        if inicio not in nodos_validos:
+            self.resultado_label.config(text=f"La estacion de inicio '{inicio}' no es una estación válida")
+            self.tiempo_label.config(text="")
+            return 
+        if destino not in nodos_validos:
+            self.resultado_label.config(text=f"La estacion destino '{destino}' no es una estación válida")
+            self.tiempo_label.config(text="")
+            return 
+        
         factor, estado = self.hora_punta(hora_str)
         if factor is None:
             return
         modulo_IA.factor_hora = factor
-        self.status_label.config(text=f"Afluencia: {estado}")
+        self.afluencia_status_label.config(text=f"Afluencia: {estado}")
         try:
             camino, tiempo_total = modulo_IA.calcular_ruta(inicio, destino)
             self.resultado_label.config(text=f"Camino: {' → '.join(camino)}")
@@ -133,7 +129,6 @@ class MetroApp:
         except Exception as e:
             self.resultado_label.config(text=f"Error inesperado: {e}")
             self.tiempo_label.config(text="")
-
 
 if __name__ == "__main__":
     root = tk.Tk()
