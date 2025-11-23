984808fef76bb67d3df6070b382a325c3f42fea2
sersayen <sersayen@gmail.com>
2025-11-23 18:30:36 +0100

    redimension de la imagen

--- MetroApp.py
+++ MetroApp.py
@@ -22,13 +22,19 @@ class MetroApp:
 
         estaciones = list(Graph.nodes)
 
+
+
         tk.Label(frame_controles, text="Inicio ruta:", bg="white").grid(row=0, column=0, padx=5, pady=5, sticky="w")
         self.estacion_inicio = ttk.Combobox(frame_controles, values=estaciones)
         self.estacion_inicio.grid(row=0, column=1, padx=5, pady=5)
 
+
         tk.Label(frame_controles, text="Final ruta:", bg="white").grid(row=1, column=0, padx=5, pady=5, sticky="w")
         self.estacion_destino = ttk.Combobox(frame_controles, values=estaciones)
         self.estacion_destino.grid(row=1, column=1, padx=5, pady=5)
+
+
+
 
         calcular_btn = tk.Button(frame_controles, text="Calcular ruta", command=self.calcular_ruta)
         calcular_btn.grid(row=2, column=0, columnspan=2, pady=10, sticky="ew")
@@ -39,13 +45,28 @@ class MetroApp:
         self.tiempo_label = tk.Label(frame_controles, text="", wraplength=250, anchor="center", justify="center", bg="white")
         self.tiempo_label.grid(row=4, column=0, columnspan=2, pady=10, sticky="ew")
 
+
+
+        #Imagen plano metro
         image_path = "Metro.png"
         image = Image.open(image_path)
-        image = image.resize((int(screen_width * 2 / 3), int(screen_height * 2 / 3)), Image.LANCZOS)
+
+        image_width, image_height = image.size
+        max_width = int(screen_width * 3/4)
+        max_height = int(screen_height * 3/4)
+
+        ratio = min(max_width / image_width, max_height / image_height)
+        
+        new_width = int(image_width * ratio)
+        new_height = int(image_height * ratio)
+        
+        image = image.resize((new_width, new_height), Image.LANCZOS)
         photo = ImageTk.PhotoImage(image)
         label = tk.Label(root, image=photo, bg="white")
         label.image = photo
         label.pack(side="right", expand=True)
+
+
 
     def calcular_ruta(self):
         inicio = self.estacion_inicio.get()
